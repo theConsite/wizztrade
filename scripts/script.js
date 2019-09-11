5a0abf0e9a10e622198f295fad90ac7c6dfaece8
Unknown <marrcel12.marga@gmail.com>
2019-09-11 21:48:57 +0200

    kupowanie + font awesome

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+
+
+
+bilety=document.getElementsByClassName('buyTicket')
+
+//bilet 1 
+bilety[0].addEventListener("click", function(){
+    console.log('b1')
+  });
+
+  //bilet 2 
+bilety[1].addEventListener("click", function(){
+    console.log('b2')
+  });
